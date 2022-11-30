6bd064e441beb3c2cba09eca36d5780d5de072f9
Nicolas Tardieu <ntardieu.contact@gmail.com>
2022-11-30 21:55:03 +0100

    feat(behavioral): pattern mediator

---
+++ behavioral/mediator.ts
@@ -0,0 +1,30 @@
+class Airplane {
+    land() { }
+}
+
+class Runway {
+    clear!: boolean;
+}
+
+// mediator
+class Tower {
+    clearForLanding(runway: Runway, plane: Airplane){
+        if(runway.clear){
+            console.log(`Plane ${plane} is clear for landing`);
+        }
+    }
+}
+
+const runway25A = new Runway()
+const runway25B = new Runway()
+const runway7 = new Runway()
+
+const a = new Airplane()
+const b = new Airplane()
+const c = new Airplane()
+
+
+// Exemple 2 : 
+
+import express from 'express';
+const app = express();
